5458a87bd14e742edf9078016a0f45ebdee5e9a8
srinivaskcg <srinivaskgubbala@gmail.com>
2016-03-10 02:53:24 -0500

    Added parameters

--- PLPProj1.cpp
+++ PLPProj1.cpp
@@ -9,53 +9,55 @@ using namespace std;
 
 string openFile(char* fileName){
 	
-	string fileContent;
-	string nextLine;
+	string file_content;
+	string next_line;
+	ifstream input_file;
 
-	ifstream inputFile;
-
-	//cout << fileName;
-
-	inputFile.open(fileName);
+	input_file.open(fileName);
 	
-	if(inputFile.fail())
+	if(input_file.fail())
 		cerr << "Error: Problem opening input file";
 
-	if(inputFile.is_open()){
-		while(inputFile.good()){
-			getline(inputFile,nextLine);
-			fileContent +=  nextLine + "\n";
+	if(input_file.is_open()){
+		while(input_file.good()){
+			getline(input_file,next_line);
+			file_content +=  next_line + "\n";
 		}
 	}
-	//cout << fileContent << endl;
-
-	return fileContent;
+	return file_content;
 }
 
-int main(int argc,char *argv[]) {
-	if(argc < 3){
-		//cout<< "Usage: p1 -ast fileLocation"<<endl;
-		return 0;
-	}
-	int i;
-	char* input;
-	bool astFound = false;
-	for(int i=0;i<argc;i++){
-		//cout <<argv[i]<<endl;
-		if(strcmp(argv[i],"-ast") == 0){
-			astFound = true;
-			break;
-		}
-	}
-	if(astFound == true){
-		
-		input = argv[2];
-		//cout << input << endl;
-		string codeString = openFile(input);
 
-		if(codeString.size() == 0)
+int main(int argc,char *argv[]) {
+
+	int i;
+	char* file_name;
+	bool ast_switch = false;
+	bool l_switch = false;
+
+	if(argc == 4){
+		if(((string) argv[1]).compare("-l") == 0)
+			l_switch = true;
+		if(((string) argv[2]).compare("-ast") == 0)
+			ast_switch = true;
+		file_name = argv[3];
+	}
+	else if(argc == 3){
+		if(((string) argv[1]).compare("-ast") == 0)
+			ast_switch = true;
+		file_name = argv[2];
+	}
+
+	if(ast_switch){
+		string code_string = openFile(file_name);
+		if(code_string.size() == 0)
 			return 0;
-		LexicalAnalyzer la(codeString);
+
+		if(l_switch){
+			cout << code_string << endl;
+		}
+
+		LexicalAnalyzer la(code_string);
 		RecursiveParser rp(la);
 		rp.parse();
 		rp.printTree();
